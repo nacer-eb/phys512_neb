e1273d893a33ee9a43c1680b4d0a7c6b3bda6073
nacer <nacerboukacem@gmail.com>
2021-09-16 04:12:44 -0400

    Fixed a bug in problem4 almost done - hmdl

--- ProblemSets/ProblemSet_01/Problem4/Problem4.py
+++ ProblemSets/ProblemSet_01/Problem4/Problem4.py
@@ -47,6 +47,7 @@ def poly_interpolate(x, x_data, y_data, deg):
         
     return y
 
+
 def cubicspln_interpolate(x, x_data, y_data, npts):
     # Turn any constant into a 1D array
     x_arr = np.atleast_1d(np.multiply(x, 1.0))
@@ -60,6 +61,57 @@ def cubicspln_interpolate(x, x_data, y_data, npts):
     return y
 
 
+def calcRational(x, p, q):
+    num = np.zeros(len(x))
+    denum = np.ones(len(x))
+    
+    for n in range(0, len(p)):
+        num += p[n]*np.power(x, n)
+    
+    for m in range(0, len(q)):
+        denum += q[m]*np.power(x, m+1) # Recall we start with x¹ not x⁰
+    return np.divide(num, denum)
+
+
+def fitRational(x, y, n, m): # n and m are size of p and q NOT orders
+    assert n+m == len(x) # make sure we have an invertible matrix
+    combined_param = np.zeros(n+m)
+    new_x = np.zeros((len(x), n+m))
+
+    # **** I went for slower but more readable code using the loops here - I might change it after
+    for i in range(len(x)):
+        for j in range(n):
+            new_x[i][j] = x[i]**j
+        for j in range(n, m+n):
+            new_x[i][j] = -y[i]*x[i]**(j+1-n)
+
+    inv_new_x = np.linalg.pinv(new_x)
+    combined_param = np.dot(inv_new_x, y)
+
+    p = combined_param[0:n]
+    q = combined_param[n:]
+    
+    print(p)
+    print(q)
+    
+    return p,q
+
+
+x = np.linspace(-1, 1, 9)
+y = 1/(1+x*x)
+
+p, q = fitRational(x, y, 5, 4)
+
+print(p, q)
+
+xx = np.linspace(-1, 1, 100)
+yy = calcRational(xx, p, q)
+
+plt.scatter(x, y)
+plt.plot(xx, yy)
+plt.show()
+
+"""
 x = np.linspace(0,3, 100)
 smaller_x = np.linspace(0, 3, 20)
 y = cubicspln_interpolate(x, smaller_x, np.sin(smaller_x), 4)
@@ -75,4 +127,4 @@ y = poly_interpolate(x, smaller_x, np.sin(smaller_x), 4)
 plt.plot(x, y-np.sin(x))
 # plt.scatter(smaller_x, np.sin(smaller_x))
 plt.show()
-
+"""
