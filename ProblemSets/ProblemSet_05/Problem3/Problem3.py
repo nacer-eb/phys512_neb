89d4e25ee3fa0e50c8bd300d72fe0fc48d272f72
nacer <nacerboukacem@gmail.com>
2021-10-29 22:42:13 -0400

    Done with problem 3

--- ProblemSets/ProblemSet_05/Problem3/Problem3.py
+++ ProblemSets/ProblemSet_05/Problem3/Problem3.py
@@ -9,9 +9,11 @@ sys.path.insert(1, "../Problem2")
 from Problem1 import apply_shift
 from Problem2 import apply_correlation
 
+# Obtain a sample gaussian
 x = np.arange(-5, 5, 0.1)
 gaussian = np.exp(-0.5*x**2)
 
+# Shift the gaussian linearly and plot a correlation at each step
 for i in range(0, 5):
     gaussian_shifted = apply_shift(gaussian, i*15)
     corr = apply_correlation(gaussian, gaussian_shifted)
