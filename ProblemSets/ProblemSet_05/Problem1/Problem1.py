5202555993488173a9d7ddf9c44200d0fcaead2a
nacer <nacerboukacem@gmail.com>
2021-10-29 22:29:33 -0400

    Problem 1 complete

--- ProblemSets/ProblemSet_05/Problem1/Problem1.py
+++ ProblemSets/ProblemSet_05/Problem1/Problem1.py
@@ -2,27 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def apply_shift(f_x, m, method = 0):
+def apply_shift(f_x, m):
+    """
+    Applies a shift to f_x
+    
+    :param f_x: Array containing f(x) values
+    :param m: The shift size
 
+    :return: The shifted array f_x
+    """
+    # Obtain the length of the input array
     N = len(f_x)
-    sigma_m = np.zeros(N)
-    sigma_m[m] = 1
 
+    # Setup the kronecker delta
+    delta_m = np.zeros(N)
+    delta_m[m] = 1
+
+    # Setup and array to store the shifted f_x
+    # Convolution with a kronecker delta leads to a shift.
     f_x_shifted = np.zeros(N)
     for x in range(0, N):
         for chi in range(0, N):
-            f_x_shifted[x] += sigma_m[chi]*f_x[x-chi]
+            f_x_shifted[x] += delta_m[chi]*f_x[x-chi]
 
     return f_x_shifted
 
 
-# Output the answer for problem 1
 def main():
+    """
+    Output the answer to problem 1
+    
+    :return: None
+    """
+
+    # Obtain a gaussian
     x = np.arange(-5, 5, 0.1)
     gaussian = np.exp(-0.5*(x)**2)
 
+    # Shift the gaussian
     gaussian_shifted = apply_shift(gaussian, 50)
-    
+
+    # Plot the shifted gaussian along with the inital gaussian
     plt.plot(gaussian, color="blue", label="initial")
     plt.plot(gaussian_shifted, color="orange", label="shifted")
     plt.legend()
