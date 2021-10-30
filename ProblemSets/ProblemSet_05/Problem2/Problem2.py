70a71b63bf70a989b92e5a4acda9c5f3ee3020b5
nacer <nacerboukacem@gmail.com>
2021-10-29 23:38:29 -0400

    Small typo correction in the comments of p2

--- ProblemSets/ProblemSet_05/Problem2/Problem2.py
+++ ProblemSets/ProblemSet_05/Problem2/Problem2.py
@@ -3,22 +3,37 @@ import matplotlib.pyplot as plt
 
 
 def apply_correlation(f, g):
+    """
+    Compute the correlation between f, g
+    
+    :param f: Array containing f(x) values
+    :param g: Array containing g(x) values
+
+    :return: The correlation of f and g 
+    """
+    # Compute the fft of f and g
     f_fft, g_fft = np.fft.fft(f), np.fft.fft(g)
 
+    # Compute the conjugate of g_fft
     conj_g_fft = np.conjugate(g_fft)
 
+    # Compute the correlation using the fourier space shortcut
     f_g_correlation = np.real(np.fft.ifft(f_fft*conj_g_fft))
 
+    # Return the correlation
     return f_g_correlation
 
 
 # Output answer for problem 2
 def main():
+    # Setup the trial gaussian function
     x = np.arange(-5, 5, 0.1)
     f = np.exp(-0.5*x**2)
-    
+
+    # Apply the self-correlation
     corr = apply_correlation(f, f)
-    
+
+    # Plot the correlation
     plt.plot(f, label="f")
     plt.plot(corr, label="correlation")
     plt.legend()
