212e87385b06a2e71d4e4eb7715e8585149c6806
nacer <nacerboukacem@gmail.com>
2021-10-29 23:37:51 -0400

    Problem 6, Done.

--- ProblemSets/ProblemSet_05/Problem6/Problem6.py
+++ ProblemSets/ProblemSet_05/Problem6/Problem6.py
@@ -3,28 +3,35 @@ import matplotlib.pyplot as plt
 import scipy.optimize as optimize
 import matplotlib
 
+# Define a bigger font for the next plots
 font = {'family' : 'normal',
         'weight' : 'bold',
         'size'   : 16}
 
 matplotlib.rc('font', **font)
 
+# Define the number of x points and frequency samples to use
 N = 10000
 N_k = N*100
 
+# Obtain a random walk and it's autocorrelation
 rnd_walk = np.cumsum(np.random.randn(4*N))[N:-N]
 rnd_walk_corr = np.correlate(rnd_walk, rnd_walk, mode='same')
 
+# Apply a hanning window to the random walk correlation
 y = np.hanning(2*N)*rnd_walk_corr
 
+# Obtain the FFT for the rw autocorrelation and the frequencies used
 y_fft = np.abs(np.fft.rfft(y, n=N_k)[1:int(N_k/100)])
 y_fft_freq = np.fft.rfftfreq(N_k)[1:int(N_k/100)]
 
+# Plot the windowed rw autocorrelation (to show it's roughly abs(x))
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 plt.plot(y)
 plt.title("Windowed Correlation")
 plt.savefig("windowed_correlation")
 
+# Plot the power spectra - FFT of the rw autocorr.
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 plt.plot(y_fft_freq, y_fft, color="orange")
 plt.title("Fourier transform of Windowed-Correlation")
@@ -33,12 +40,15 @@ plt.ylabel("Fourier amplitude")
 plt.savefig("rw_ps")
 
 
-mean_coef = np.mean(y_fft*y_fft_freq**2)
-
+# Plot the power spectra * k² to show that is constant (roughly)
+# This implies the power spectra goes like k^-2
 fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 plt.plot(y_fft_freq, y_fft*y_fft_freq**2, color="orange")
+
+mean_coef = np.mean(y_fft*y_fft_freq**2)
 plt.plot([np.min(y_fft_freq), np.max(y_fft_freq)],
          [mean_coef, mean_coef], color="red")
+
 plt.ylabel("Fourier transform times $k^2$")
 plt.xlabel("Frequency")
 plt.savefig("fft_fourier_ksqrd_analysis")
